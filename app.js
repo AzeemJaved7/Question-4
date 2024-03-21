6b3cd90e61ffff3ac1d2ceb5c2aef3b947e5902f
AzeemJaved7 <163923406+AzeemJaved7@users.noreply.github.com>
2024-03-21 06:04:19 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+"use strict";
+let AuthorName = "Albert Einstein";
+let message = `${AuthorName} once said, A person who never made a mistake never tried anything new`;
+console.log(message);
---
+++ app.ts
@@ -0,0 +1,3 @@
+let AuthorName: string= "Albert Einstein";
+let message: string= `${AuthorName} once said, A person who never made a mistake never tried anything new`;
+console.log(message);
